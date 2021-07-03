3e3d80643920d16ce4b0b44e3e9e05949e28db44
Ulrik de Muelenaere <ulrikdem@gmail.com>
2021-07-03 23:09:43 +0200

    Swap outer navigation columns

--- keymap.c
+++ keymap.c
@@ -100,30 +100,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [NAV] = LAYOUT_split_3x5_3(
-        DM_REC1, DM_PLY1, TG(GAM), XXXXXXX, XXXXXXX,                   KC_VOLU, KC_HOME, KC_UP,   KC_END,  KC_PGUP,
-        OSM_ALT, OSM_GUI, OSM_SFT, OSM_CTL, XXXXXXX,                   KC_VOLD, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,
-        RESET,   XXXXXXX, LOCK,    TG(MOU), XXXXXXX,                   KC_MUTE, KC_INS,  KC_CAPS, KC_APP,  RESET,
+        DM_REC1, DM_PLY1, TG(GAM), XXXXXXX, RESET,                     KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU,
+        OSM_ALT, OSM_GUI, OSM_SFT, OSM_CTL, XXXXXXX,                   KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD,
+        XXXXXXX, XXXXXXX, LOCK,    TG(MOU), XXXXXXX,                   XXXXXXX, KC_INS,  KC_CAPS, KC_APP,  KC_MUTE,
                                    _______, _______, KC_DEL,  KC_BSPC, _______, _______
     ),
 
     [MOU] = LAYOUT_split_3x5_3(
-        _______, _______, _______, _______, _______,                   _______, KC_WH_L, KC_MS_U, KC_WH_R, KC_WH_U,
-        _______, _______, _______, _______, _______,                   _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,
+        _______, _______, _______, _______, _______,                   KC_WH_U, KC_WH_L, KC_MS_U, KC_WH_R, _______,
+        _______, _______, _______, _______, _______,                   KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, _______,
         _______, _______, _______, _______, _______,                   _______, KC_BTN4, KC_BTN5, _______, _______,
                                    _______, _______, _______, KC_BTN3, KC_BTN1, KC_BTN2
     ),
 
     [GAM] = LAYOUT_split_3x5_3(
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,                      KC_VOLU, KC_HOME, KC_UP,   KC_END,  KC_PGUP,
-        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,                      KC_VOLD, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,
-        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,                      KC_MUTE, KC_BTN1, KC_BTN3, KC_BTN2, DM_PLY1,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,                      KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU,
+        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD,
+        KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,                      DM_PLY1, KC_BTN1, KC_BTN3, KC_BTN2, KC_MUTE,
                                    LT_GESC, KC_SPC,  LOCK,    KC_BSPC, TG(GAM), KC_ENT
     ),
 
     [GNUM] = LAYOUT_split_3x5_3(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F11,
-        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      XXXXXXX, _______, _______, _______, KC_F12,
+        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      _______, _______, _______, _______, KC_F12,
                                    _______, _______, _______, _______, _______, _______
     ),
 };
