af90d2cca8f3228cfbfbc87a5aa21be94e991d07
Ulrik de Muelenaere <ulrikdem@gmail.com>
2021-08-04 21:18:59 +0200

    Add mirror key to more layers

--- keymap.c
+++ keymap.c
@@ -46,6 +46,7 @@ enum custom_keycodes {
 #define MT_6 RGUI_T(KC_6)
 #define MT_0 LALT_T(KC_0)
 #define LT_F10 LT(MIR, KC_F10)
+#define LT_PSLS LT(MIR, KC_PSLS)
 
 #define MT_BSLS LALT_T(ENCODE_SYM(KC_BSLS))
 #define MT_UNDS LGUI_T(ENCODE_SYM(KC_UNDS))
@@ -137,7 +138,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYER(KP,
         _______, _______, _______, _______, _______,                   KC_PAST, KC_P7,   KC_P8,   KC_P9,   KC_PDOT,
         _______, _______, _______, _______, KC_NLCK,                   KC_PMNS, KC_P4,   KC_P5,   KC_P6,   KC_P0,
-        _______, _______, _______, _______, _______,                   KC_PPLS, KC_P1,   KC_P2,   KC_P3,   KC_PSLS,
+        _______, _______, _______, _______, _______,                   KC_PPLS, KC_P1,   KC_P2,   KC_P3,   LT_PSLS,
                                    _______, _______, _______, _______, _______, KC_PENT
     ),
 
@@ -172,7 +173,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYER(GNUM,
         DM_PLY1, KC_9,    KC_8,    KC_7,    KC_T,                      XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F12,
         KC_0,    KC_6,    KC_5,    KC_4,    KC_G,                      XXXXXXX, KC_F4,   KC_F5,   KC_F6,   KC_F11,
-        _______, KC_3,    KC_2,    KC_1,    KC_B,                      XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F10,
+        OSL_MIR, KC_3,    KC_2,    KC_1,    KC_B,                      XXXXXXX, KC_F1,   KC_F2,   KC_F3,   LT_F10,
                                    KC_ESC,  _______, _______, _______, _______, KC_ENT
     ),
 };
