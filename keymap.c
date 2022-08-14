f76554feda44ff14f579c7a09c34b162a717a82a
Ulrik de Muelenaere <ulrikdem@gmail.com>
2022-08-13 20:23:57 -0400

    Duplicate mirror keys in bottom row

--- keymap.c
+++ keymap.c
@@ -98,7 +98,7 @@ bool caps_word_press_user(uint16_t keycode) {
     k10,   k11,   k12,   k13,   k14,   k15,   KC_NO,               KC_NO, k16,   k17,   k18,   k19,   k1a,   k1b, \
     k20,   k21,   k22,   k23,   k24,   k25,   KC_NO,               KC_NO, k26,   k27,   k28,   k29,   k2a,   k2b, \
     k30,   k31,   k32,   k33,   k34,   k35,   KC_NO, KC_NO, KC_NO, KC_NO, k36,   k37,   k38,   k39,   k3a,   k3b, \
-    KC_NO, KC_NO, KC_NO, KC_NO,        k43,   k44,   k45,   k46,   k47,   k48,          KC_NO, KC_NO, KC_NO, KC_NO \
+    k30,   KC_NO, KC_NO, KC_NO,        k43,   k44,   k45,   k46,   k47,   k48,          KC_NO, KC_NO, KC_NO, k3b \
 )
 
 #define LAYER(layer, \
