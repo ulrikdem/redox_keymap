a73cb049d0008b8bb0c8eb327610ac214cb6fb45
Ulrik de Muelenaere <ulrikdem@gmail.com>
2023-04-13 23:45:13 -0400

    Move gaming layer macro record key

--- keymap.c
+++ keymap.c
@@ -220,7 +220,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_1,    KC_2,    KC_3,    KC_4,                                        KC_BTN1, KC_BTN3, KC_BTN2, KC_BSPC,
         KC_TAB,  KC_LALT, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_F7,   KC_F8,   KC_F9,   KC_F12,  KC_BSLS,
         KC_ESC,  KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_F4,   KC_F5,   KC_F6,   KC_F11,  KC_QUOT,
-        OSL_MIR, KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    DM_REC2, KC_F1,   KC_F2,   KC_F3,   KC_F10,  OSL_MIR,
+        OSL_MIR, KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_F1,   KC_F2,   KC_F3,   KC_F10,  OSL_MIR,
                                    LT_GLOC, KC_SPC,  LT_PLY2, CLEAR,   LT_SPC,  KC_ENT
     ),
 
@@ -229,7 +229,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     LAYER(GAME_NUM,
         KC_F1,   KC_F2,   KC_F3,   KC_F4,                                       _______, _______, _______, _______,
-        _______, _______, KC_7,    KC_8,    KC_9,    XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______,
+        _______, _______, KC_7,    KC_8,    KC_9,    DM_REC2, XXXXXXX, _______, _______, _______, _______, _______,
         _______, _______, KC_4,    KC_5,    KC_6,    KC_0,    XXXXXXX, _______, _______, _______, _______, _______,
         _______, _______, KC_1,    KC_2,    KC_3,    KC_A,    XXXXXXX, _______, _______, _______, _______, _______,
                                    _______, LT_GSPC, LT_MPLY, _______, _______, _______
