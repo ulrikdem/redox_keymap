8f15358dd0553d289c736ca45d36c62b4796b247
Ulrik de Muelenaere <ulrikdem@gmail.com>
2023-04-13 23:45:30 -0400

    Add gaming navigation layer

--- keymap.c
+++ keymap.c
@@ -85,6 +85,7 @@ _Static_assert((uint16_t)SFT_SYM_MAX < KC_MUTE);
 enum layers {
     GAME,
     GAME_NUM,
+    GAME_NAV,
     BASE,
     SFT,
     NUM,
@@ -214,14 +215,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Gaming Layers {{{1
 
 #define LT_GLOC LT(GAME_NUM, LOCK)
+#define LT_GESC LT(GAME_NAV, KC_ESC)
 #define LT_PLY2 LT(BASE, PLY2)
 
     LAYER(GAME,
         KC_1,    KC_2,    KC_3,    KC_4,                                        KC_BTN1, KC_BTN3, KC_BTN2, KC_BSPC,
         KC_TAB,  KC_LALT, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_F7,   KC_F8,   KC_F9,   KC_F12,  KC_BSLS,
-        KC_ESC,  KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_F4,   KC_F5,   KC_F6,   KC_F11,  KC_QUOT,
+        LT_GESC, KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_F4,   KC_F5,   KC_F6,   KC_F11,  KC_QUOT,
         OSL_MIR, KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_F1,   KC_F2,   KC_F3,   KC_F10,  OSL_MIR,
-                                   LT_GLOC, KC_SPC,  LT_PLY2, CLEAR,   LT_SPC,  KC_ENT
+                                   LT_GLOC, KC_SPC,  LT_PLY2, CLEAR,   KC_SPC,  KC_ENT
     ),
 
 #define LT_GSPC LT(BASE, KC_SPC)
@@ -233,6 +235,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, KC_4,    KC_5,    KC_6,    KC_0,    XXXXXXX, _______, _______, _______, _______, _______,
         _______, _______, KC_1,    KC_2,    KC_3,    KC_A,    XXXXXXX, _______, _______, _______, _______, _______,
                                    _______, LT_GSPC, LT_MPLY, _______, _______, _______
+    ),
+
+    LAYER(GAME_NAV,
+        KC_F5,   KC_F6,   KC_F7,   KC_F8,                                       _______, _______, _______, _______,
+        _______, _______, KC_HOME, KC_UP,   KC_END,  KC_VOLU, XXXXXXX, _______, _______, _______, _______, _______,
+        _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, XXXXXXX, _______, _______, _______, _______, _______,
+        _______, _______, REPEAT,  KC_BSPC, KC_DEL,  KC_MUTE, XXXXXXX, _______, _______, _______, _______, _______,
+                                   _______, KC_ENT,  _______, _______, _______, _______
     ),
 
 };
